0f4464db58bf489df5c3e8b5c64d78e3860015cb
Arek Szatkowski <muczaczos@gmail.com>
2019-12-11 21:00:53 +0100

    Updates

---
+++ precache-manifest.51dbd40d410954c6703e0915af3e9ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6104a20cac86eb204d0bed1f65c847",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c88ab7eab29420c1c2b",
+    "url": "/monsters-rolodex/static/css/main.604f6e7f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c88ab7eab29420c1c2b",
+    "url": "/monsters-rolodex/static/js/main.a8e0d8d3.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
